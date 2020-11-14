fb8470423078cecc9bc969810decf0e157700b85
jmaslanka55 <59845680+jmaslanka55@users.noreply.github.com>
2020-11-14 15:40:30 -0500

    Basic Formatting and creating components for courses and schedules

---
+++ angular-jmaslank-lab4/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Routes, RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {ListComponent} from "./list/list.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
