42cefc7112997ba004df4c25c0aeb917ed43770a
jmaslanka55 <59845680+jmaslanka55@users.noreply.github.com>
2020-11-14 15:32:50 -0500

    Styling and setting up routing

--- angular-jmaslank-lab4/src/app/app.module.ts
+++ angular-jmaslank-lab4/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, RouterModule, AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
