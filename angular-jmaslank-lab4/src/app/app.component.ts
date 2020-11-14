6f888157c001315fc578812e3f8ba4da925d3b48
jmaslanka55 <59845680+jmaslanka55@users.noreply.github.com>
2020-11-14 17:28:33 -0500

    Connected Backend

--- angular-jmaslank-lab4/src/app/app.component.ts
+++ angular-jmaslank-lab4/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {HttpService} from "./http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-jmaslank-lab4';
+  constructor(private service : HttpService){
+
+  }
+  ngOnInit(){
+    this.getDataFromAPI();
+  }
+  getDataFromAPI(){
+    this.service.getSubjects().subscribe((response) => {
+      console.log('response is ', response)
+    }, (error) =>{
+      console.log(error);
+    })
+  }
 }
