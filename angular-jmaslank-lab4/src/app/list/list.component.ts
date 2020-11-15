8b7ff6b05568d75c94aee9812e6c2573bec3682c
jmaslanka55 <59845680+jmaslanka55@users.noreply.github.com>
2020-11-15 18:49:28 -0500

    Styling for course search

--- angular-jmaslank-lab4/src/app/list/list.component.ts
+++ angular-jmaslank-lab4/src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppComponent} from "../app.component";
 import {HttpService} from "../http.service";
 
@@ -9,40 +9,69 @@ import {HttpService} from "../http.service";
 })
 export class ListComponent implements OnInit {
   public arr: String[] = [];
-  constructor(private service: HttpService) { }
+
+  constructor(private service: HttpService) {
+  }
+
   subjectVal;
   courseVal;
   component;
+
   ngOnInit(): void {
   }
 
-  clearDisplay(){
-    document.getElementById("AllSubjects").textContent ="";
+  clearDisplay() {
     document.getElementById("DisplaySearch").textContent = "";
+    let list = document.getElementById("allClasses");
+    while (list.hasChildNodes()) {
+      list.removeChild(list.firstChild);
+    }
+    let list2 = document.getElementById("results");
+    while (list2.hasChildNodes()) {
+      list2.removeChild(list.firstChild);
+    }
   }
-  getSubjects(){
+
+  getSubjects() {
     return this.service.get('api/subject')
   }
-  getData(){
-    this.getSubjects().subscribe((res : any) =>{
+
+  getData() {
+    this.getSubjects().subscribe((res: any) => {
       this.arr = res;
-      document.getElementById("AllSubjects").textContent = JSON.stringify(this.arr);
+      for (let i = 0; i < Object.keys(this.arr).length / 2; i++) {
+        let list = document.createElement('li');
+        let subjectNode = document.createTextNode(res["Subject code " + (i + 1)] + ": ");
+        let nameNode = document.createTextNode(res["className " + (i + 1)]);
+        list.appendChild(subjectNode);
+        list.appendChild(nameNode);
+        document.getElementById("allClasses").appendChild(list);
+      }
     })
   }
-  getSubjectCourse(){
+
+  getSubjectCourse() {
     return this.service.get(`api/timetable/${this.subjectVal}/${this.courseVal}`);
   }
-  searchSubjectCourse(){
-    this.getSubjectCourse().subscribe((res:any)=>{
-      res = JSON.stringify(res[0].className) + JSON.stringify(res[0].catalog_description) + JSON.stringify(res[0].course_info[0].start_time);
+
+  searchSubjectCourse() {
+    this.getSubjectCourse().subscribe((res: any) => {
+      res = JSON.stringify(res[0].className) + JSON.stringify(res[0].catalog_nbr) + JSON.stringify(res[0].catalog_description) + JSON.stringify(res[0].course_info[0].start_time)
+        + JSON.stringify(res[0].course_info[0].ssr_component);
       document.getElementById("DisplaySearch").textContent = res;
+
+    }, error => {
+      console.log(error);
     })
   }
-  getSubjectCourseComponent(){
+
+
+  getSubjectCourseComponent() {
     return this.service.get(`api/timetable/${this.subjectVal}/${this.courseVal}/${this.component}`);
   }
-  searchSubjectCourseComponent(){
-    this.getSubjectCourseComponent().subscribe((res: any)=>{
+
+  searchSubjectCourseComponent() {
+    this.getSubjectCourseComponent().subscribe((res: any) => {
       res = JSON.stringify(res[0].className) + JSON.stringify(res[0].catalog_nbr) + JSON.stringify(res[0].catalog_description) + JSON.stringify(res[0].course_info[0].start_time)
         + JSON.stringify(res[0].course_info[0].ssr_component);
       document.getElementById("DisplaySearch").textContent = res;
