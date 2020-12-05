ff213fda1e999178aca5267069363d60b1adc33d
jmaslanka55 <59845680+jmaslanka55@users.noreply.github.com>
2020-12-04 23:00:56 -0500

    Show timetable for user's list of courses

--- angular-jmaslank-lab4/src/app/home/home.component.ts
+++ angular-jmaslank-lab4/src/app/home/home.component.ts
@@ -26,14 +26,17 @@ export class HomeComponent implements OnInit {
     })
   };
   readonly schedule = {};
-  constructor(private service: HttpService,private router: Router) {
+
+  constructor(private service: HttpService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
-  populateCodes(){
+
+  populateCodes() {
     return this.service.get(`api/courses/${this.subjectName}`)
   }
+
   populate() {
     this.populateCodes().subscribe((res: any) => {
       this.arr = res;
@@ -41,120 +44,116 @@ export class HomeComponent implements OnInit {
   }
 
 
-  addToSchedule(course,auth:string) {
-    return this.service.put(`api/make/schedule/${this.schedName}/${auth}`,{"subject":this.subjectName, "catalog_nbr": course} , {responseType: 'text'});
+  addToSchedule(course, auth: string) {
+    return this.service.put(`api/make/schedule/${this.schedName}/${auth}`, {
+      "subject": this.subjectName,
+      "catalog_nbr": course
+    }, {responseType: 'text'});
   }
 
   addingSchedule(course) {
-    if(this.subjectName == "" || course == null){
+    if (this.subjectName == "" || course == null) {
       alert("Invalid Subject/Code");
       return;
     }
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
-    this.addToSchedule(course,JSON.stringify(authObject)).subscribe((res: any) => {
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
+    this.addToSchedule(course, JSON.stringify(authObject)).subscribe((res: any) => {
       let temp = JSON.parse(res.body);
-      if (temp.message == "failed"){
+      if (temp.message == "failed") {
         this.router.navigate(['']);
       }
       console.log(res);
     })
   }
 
-
-
-
-
-  deleteAllSchedules() {
-    return this.service.post(`api/schedulelist`,this.schedule,{responseType: 'text'})
-  }
-  deleteAll(){
-    this.deleteAllSchedules().subscribe((res:any)=>{
-      console.log(res);
-    })
+  setPublic(auth: string) {
+    return this.service.post(`api/set/public/${this.schedName}/${auth}`, this.schedule, {responseType: 'text'});
   }
 
-  listSchedules() {
-    return this.service.get(`api/show/schedule`);
-  }
-
-  listAllSchedules() {
-    this.listSchedules().subscribe((res: any) => {
-      document.getElementById("ShowResults").textContent = JSON.stringify(res);
-    })
-  }
-  setPublic(auth:string){
-    return this.service.post(`api/set/public/${this.schedName}/${auth}`, this.schedule,{responseType: 'text'});
-  }
-  publicCall(){
-    if(this.subjectName == ""){
+  publicCall() {
+    if (this.subjectName == "") {
       alert("Invalid Subject/Code");
       return;
     }
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
     this.setPublic(JSON.stringify(authObject)).subscribe((res: any) => {
       let temp = JSON.parse(res.body);
-      if (temp.message == "failed"){
+      if (temp.message == "failed") {
         this.router.navigate(['']);
       }
     })
   }
-  makeDesc(auth:string){
-    return this.service.put(`api/make/description/${this.schedName}/${auth}`,{"description": this.description}, {responseType: 'text'});
+
+  makeDesc(auth: string) {
+    return this.service.put(`api/make/description/${this.schedName}/${auth}`, {"description": this.description}, {responseType: 'text'});
   }
-  descCall(){
+
+  descCall() {
     if (this.description == "") {
       alert("Invalid description")
       return;
     }
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
     this.makeDesc(JSON.stringify(authObject)).subscribe((res: any) => {
       let temp = JSON.parse(res.body);
-      if (temp.message == "failed"){
+      if (temp.message == "failed") {
         this.router.navigate(['']);
       }
     })
   }
-  makeSchedule(auth:string) {
-    return this.service.put(`api/schedule/${this.schedName}/${auth}`, this.schedule, {responseType: 'text', observe: 'response'})
+
+  makeSchedule(auth: string) {
+    return this.service.put(`api/schedule/${this.schedName}/${auth}`, this.schedule, {
+      responseType: 'text',
+      observe: 'response'
+    })
   }
 
   createSchedule() {
-    if (this.schedName == ""){
+    if (this.schedName == "") {
       alert("Invalid Name for List")
       return;
     }
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
     this.makeSchedule(JSON.stringify(authObject)).subscribe((res: any) => {
       let temp = JSON.parse(res.body);
-      if (temp.message == "failed"){
+      if (temp.message == "failed") {
         this.router.navigate(['']);
       }
     })
   }
-  changePass(auth:string){
-    return this.service.post(`api/updatepass/${this.password}/${this.email}/${auth}`, {newPass:this.newpassword},{responseType: 'text', observe: 'response'});
+
+  changePass(auth: string) {
+    return this.service.post(`api/updatepass/${this.password}/${this.email}/${auth}`, {newPass: this.newpassword}, {
+      responseType: 'text',
+      observe: 'response'
+    });
   }
-  callChangePass(){
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
+
+  callChangePass() {
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
     this.changePass(JSON.stringify(authObject)).subscribe((res: any) => {
     })
   }
-  showUserCourses(auth:string){
+
+  showUserCourses(auth: string) {
     return this.service.get(`api/show/lists/${auth}`)
   }
-  callUserCourses(){
-    let authObject = {headers: {Authorization : "Bearer " + localStorage.getItem("jwt")}};
-    this.showUserCourses(JSON.stringify(authObject)).subscribe((res:any)=>{
+
+  callUserCourses() {
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
+    this.showUserCourses(JSON.stringify(authObject)).subscribe((res: any) => {
       this.lists = res;
     })
   }
-  removeSchedule(auth:string,schedName:string) {
-    return this.service.post(`api/remove/schedule/${schedName}/${auth}`,this.schedule, {responseType: 'text'});
+
+  removeSchedule(auth: string, schedName: string) {
+    return this.service.post(`api/remove/schedule/${schedName}/${auth}`, this.schedule, {responseType: 'text'});
   }
 
   deleteSchedule(schedName) {
     let confirmation = prompt("Please type CONFIRM to confirm deletion of list");
-    if (confirmation =="CONFIRM") {
+    if (confirmation == "CONFIRM") {
       let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
       this.removeSchedule(JSON.stringify(authObject), schedName).subscribe((res: any) => {
       })
@@ -162,13 +161,34 @@ export class HomeComponent implements OnInit {
       alert("Not Deleted");
     }
   }
-  leaveReview(auth:string,courseCode){
-    return this.service.post(`api/review/create/${auth}`, {subject: this.subjectName, courseCode: courseCode, review: this.review},{responseType: 'text'} )
-  }
-  callReview(courseCode){
-    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
-    this.leaveReview(JSON.stringify(authObject),courseCode).subscribe((res: any) => {
 
+  leaveReview(auth: string, courseCode) {
+    return this.service.post(`api/review/create/${auth}`, {
+      subject: this.subjectName,
+      courseCode: courseCode,
+      review: this.review
+    }, {responseType: 'text'})
+  }
+
+  callReview(courseCode) {
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
+    this.leaveReview(JSON.stringify(authObject), courseCode).subscribe((res: any) => {
+
+    })
+  }
+
+  DisplaySelectedSched(auth:string, list) {
+    return this.service.get(`api/display/schedule/${list}/${auth}`);
+  }
+
+  callDisplaySchedule(list) {
+    let authObject = {headers: {Authorization: "Bearer " + localStorage.getItem("jwt")}};
+    this.DisplaySelectedSched(JSON.stringify(authObject), list).subscribe((res: any) =>{
+      let classes = "";
+      for (let i = 0; i<res.length;i++){
+        classes += JSON.stringify(res[i][0].catalog_nbr)+ " Name: " + JSON.stringify(res[i][0].className) + JSON.stringify(res[i][0].catalog_description) + "\n\n"
+      }
+      document.getElementById("ShowResults").textContent = `Schedule: ${this.schedName} \n Classes: \n ` + classes;
     })
   }
 }
--- index.js
+++ index.js
@@ -529,39 +529,31 @@ app.get('/api/courses/:subject_code', (req, res) => {
 
 
 //Task 6
-app.get('/api/display/schedule/:scheduleName', (req, res) => {
-    let schedName = req.sanitize(req.params.scheduleName);
-    let showSched = [];
-    for (let i = 0; i < db.getState().Schedule.length; i++) {
-        if (db.getState().Schedule[i].schedule_name.toUpperCase() === schedName.toUpperCase()) {
-            for (let k = 0; k < db.getState().Schedule[i].course_name.length; k++) {
-                let showCourse = db.getState().Schedule[i].course_name[k];
-                let showSubject = db.getState().Schedule[i].subject[k];
-                const course = data.filter(a => a.subject.toString().toLowerCase() === req.sanitize(showSubject.toString().toLowerCase()));
-                const final = course.filter(a => a.catalog_nbr.toString().toUpperCase() === req.sanitize(showCourse.toString().toUpperCase()));
-                showSched.push(final);
+app.get('/api/display/schedule/:scheduleName/:auth_token', (req, res) => {
+    const token = req.sanitize(req.params.auth_token);
+    const jsonToken = JSON.parse(token);
+    if (authenticateJWT(jsonToken) == 101) {
+        let schedName = req.sanitize(req.params.scheduleName);
+        let showSched = [];
+        for (let i = 0; i < db.getState().Schedule.length; i++) {
+            if (db.getState().Schedule[i].schedule_name.toUpperCase() === schedName.toUpperCase()) {
+                for (let k = 0; k < db.getState().Schedule[i].course_name.length; k++) {
+                    let showCourse = db.getState().Schedule[i].course_name[k];
+                    let showSubject = db.getState().Schedule[i].subject[k];
+                    const course = data.filter(a => a.subject.toString().toLowerCase() === req.sanitize(showSubject.toString().toLowerCase()));
+                    const final = course.filter(a => a.catalog_nbr.toString().toUpperCase() === req.sanitize(showCourse.toString().toUpperCase()));
+                    showSched.push(final);
+                }
+                res.send(showSched);
+                return;
             }
-            res.send(showSched);
-            return;
         }
     }
-    res.status(404).send("Schedule not found");
 });
 
-//Allow to remove schedule by name
 
-//get list of all schedule names (Task 8)
-app.get('/api/show/schedule', (req, res) => {
-    let schedList = [];
-    for (let i = 0; i < db.getState().Schedule.length; i++) {
-        if (db.getState().Schedule[i].course_name.length > 0) {
-            schedList.push(`Schedule Name: ${db.getState().Schedule[i].schedule_name} Amount of courses: ${db.getState().Schedule[i].course_name.length}`);
-        } else {
-            schedList.push(`Schedule Name: ${db.getState().Schedule[i].schedule_name} Amount of courses: 0`)
-        }
-    }
-    res.send(schedList);
-});
+
+
 
 
 
